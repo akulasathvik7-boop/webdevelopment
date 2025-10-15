ddf8dc48cb3f1d7ed5a6c5f478aa10dff4e3c360
sathvik <akulasathvik7@gmail.com>
2025-10-15 06:58:39 +0530

    Add files via upload
    added

---
+++ index.js
@@ -0,0 +1,117 @@
+// console.log("Hello, World!");
+// const name = "Meridian";
+// var age = 25;
+// let isStudent = true;
+// let score 
+// var num=String(age);
+// console.table({name, age, isStudent, score});
+// console.log(num);
+// console.log(typeof num);
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log("02">1);
+// console.log(null<=0);
+// console.log(null>=0);
+// console.log(null>=0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);
+// console.log(undefined<0);
+// console.log("2"===2);
+// console.log("2"==2);
+// console.log(NaN===NaN);
+// console.log(NaN==NaN);
+// //Number String ̣Boolean undefined sysmbol null bigint
+// const ame= new String("Meridian");
+// console.log(ame);
+// console.log(typeof ame);
+// let x= "    sathvik    ";
+// console.log(x.length);
+// console.log(x);
+// // console.log(x.trim().length);
+// console.log(x.trimEnd().length);
+// console.log(x.trimStart().length);
+// let y=x.trim();
+// console.log(y.length);
+// console.log(y);
+// console.log(y.toUpperCase());
+// console.log(y.toLowerCase());
+// let sathvik="sathvik";
+// console.log(`my name is ${sathvik}`);
+// console.log(`my name is ${sathvik}`);
+// let a=10;
+// let b=20;
+// console.log(`the sum of ${a} and ${b} is ${a+b}`);
+// let mydate=new Date(24,1,2005);
+// console.log(typeof (mydate));
+// console.log(mydate.toString());
+// let obj={
+//     name:"sathvik",
+//     age:19,
+//     isStudent:true
+// }
+// console.log(obj);
+// console.log(typeof obj);
+// obj.name="Sathvik varma";
+// console.log(obj.name);
+// let tinyuser={}
+// tinyuser.name="sathvik";
+// tinyuser.age=19;
+// tinyuser.isStudent=true;
+// console.log(tinyuser);
+// console.log(typeof tinyuser);
+// const aboutme = {
+//     name: {
+//         fullname: {
+//             firstName:"Sathvik",
+//             lastName:"Varma"
+//         }
+//     }
+   
+  
+// }
+// console.log(aboutme.name.fullname.lastName);
+// const arr=[1,2,3,4,5];
+// const arr2=new Array(6,7,8,9,10);
+// const arr3={arr,arr2};
+// console.log(arr3);
+// console.log(typeof arr3);
+// const obj1={1:'a',2:'b',3:'c'}; 
+// const obj2={4:'d',5:'e',6:'f'};
+// const obj3=Object.assign(obj1,obj2);
+//target==obj3->obj1 is target so we keep {}->as a target
+//spread method to joining or concatenating two objects{...obj1,...obj2}
+// console.log(obj3);
+// if(obj1===obj3){
+//     console.log("true");
+// }
+// const myfun=function(){
+//     return 2+4;
+// }
+// console.log(myfun());
+// function myfun2(user){
+//     if(user==undefined){
+//         console.log("user not found");
+//         return;
+//     }
+//     return `user found ${user}`;
+// }
+// console.log(myfun2());
+// var k=30;
+// let i=90;
+// {
+//     let i=0;
+//     const j=10;
+//     var k=20;
+//     console.log(i);
+//     console.log(j);
+//     console.log(k);
+// }
+// console.log(i);
+// console.log(j);
+// console.log(k);
